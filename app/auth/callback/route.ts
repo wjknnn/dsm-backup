289b37cbcb961061eee89d89c5340004836047ce
SeungWoo <wjknn3123@gmail.com>
2024-04-26 11:40:54 +0900

    🔨 Edit :: save cookie

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -9,22 +9,28 @@ export async function GET(request: Request) {
   if (code) {
     const supabase = createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
     const cookie = cookies().get('sb-lxpaoyxnoburzcujblhu-auth-token.0')?.value;
     const userId = cookie?.split(':')[7].split('"')[1];
-    console.log(userId);
-    const { data, error: userError } = await supabase
+    const token = cookie?.split('"')[3];
+
+    cookies().set('userId', userId || '');
+    cookies().set('token', token || '');
+
+    const { error: userError } = await supabase
       .from('users')
       .select('*')
       .eq('id', userId)
       .single();
+
     if (error) {
       return NextResponse.redirect(`${requestUrl.origin}/login`);
     }
+
     if (userError) {
       return NextResponse.redirect(`${requestUrl.origin}/signup`);
     }
   }
 
-  // URL to redirect to after sign up process completes
   return NextResponse.redirect(`${requestUrl.origin}/protected`);
 }
