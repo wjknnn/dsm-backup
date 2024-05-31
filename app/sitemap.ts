90f98abe6fcacc7c386676e070b8b239368fa3a5
SeungWoo <wjknn3123@gmail.com>
2024-05-31 16:15:57 +0900

    🔧 Add :: Sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/signup`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/login`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/feedback`,
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/topic`,
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 0.8,
+    },
+  ];
+}
