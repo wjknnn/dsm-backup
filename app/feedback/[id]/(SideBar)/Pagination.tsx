355bcd7928507f9f384efea8ba1bf2db7109645f
SeungWoo <wjknn3123@gmail.com>
2024-06-11 20:28:27 +0900

    ✨ Edit :: Popular feedback pagination

--- app/feedback/[id]/(SideBar)/Pagination.tsx
+++ app/feedback/[id]/(SideBar)/Pagination.tsx
@@ -1,3 +1,45 @@
+'use client';
+
+import { Arrow } from '@/assets';
+import { popularFeedbackStore } from '@/store';
+
 export const Pagination = () => {
-  return <div></div>;
+  const { page, max, updatePage } = popularFeedbackStore();
+  return (
+    <div className="flex items-center justify-center gap-4 px-6">
+      <button
+        onClick={() => updatePage(page - 1)}
+        disabled={page === 1}
+        className={`p-2 border rounded-full border-grayLight2 hover:bg-grayLight2 ${
+          page === 1 ? 'text-grayDark1' : 'tetx-black'
+        }`}
+      >
+        <Arrow size={20} />
+      </button>
+      <div className="flex">
+        {[...new Array(max)].map((_, index) => (
+          <button
+            key={index}
+            onClick={() => updatePage(index + 1)}
+            className={`size-8 rounded-xl ${
+              page === index + 1
+                ? 'bg-black text-white text-bodyStrong'
+                : 'bg-white text-black text-body2'
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+      <button
+        onClick={() => updatePage(page + 1)}
+        disabled={page === max}
+        className={`p-2 border rounded-full border-grayLight2 hover:bg-grayLight2 ${
+          page === max ? 'text-grayDark1' : 'tetx-black'
+        }`}
+      >
+        <Arrow size={20} className="rotate-180" />
+      </button>
+    </div>
+  );
 };
--- app/feedback/[id]/(SideBar)/PopularFeedback.tsx
+++ app/feedback/[id]/(SideBar)/PopularFeedback.tsx
@@ -2,24 +2,34 @@
 
 import { getPopularFeedback } from '@/apis';
 import { FeedbackChip } from '@/components';
+import { popularFeedbackStore } from '@/store';
 import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 export const PopularFeedback = () => {
+  const { page, updateMax } = popularFeedbackStore();
+
   const { data } = useQuery({
-    queryKey: ['popular feedbak'],
+    queryKey: ['popular feedbak', page],
     queryFn: getPopularFeedback,
     staleTime: 10_000,
   });
 
+  useEffect(() => {
+    if (data) {
+      updateMax(Math.ceil(data.length / 4));
+    }
+  }, [data]);
+
   return (
     <section className="flex flex-col max-w-[320px] items-center">
-      {data?.slice(0, 4).map((value) => (
+      {data?.slice(page * 4 - 4, page * 4).map((value) => (
         <>
           <Link
             href={`/feedback/${value.id}`}
             key={value.id}
-            className="flex flex-col gap-2 px-6 py-3 hover:bg-grayLight2 hover:bg-opacity-60"
+            className="flex flex-col w-full gap-2 px-6 py-3 hover:bg-grayLight2 hover:bg-opacity-60"
           >
             <div className="flex flex-col gap-1">
               <FeedbackChip status={value.status} />
